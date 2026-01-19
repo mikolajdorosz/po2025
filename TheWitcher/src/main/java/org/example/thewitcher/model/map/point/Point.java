c0e77e0403c7c0aa138fbb9ba90ad792cc443503
mikolajdorosz <mikolajdorosz@gmail.com>
2026-01-19 10:29:22 +0100

    Added layering to Point class

--- TheWitcher/src/main/java/org/example/thewitcher/model/map/point/Point.java
+++ TheWitcher/src/main/java/org/example/thewitcher/model/map/point/Point.java
@@ -1,20 +1,29 @@
 package org.example.thewitcher.model.map.point;
 
 public class Point {
-    protected char marker;
-    protected int x;
-    protected int y;
-    protected boolean isObstacle;
+    private final char baseMarker;
+    private Character overlayMarker;
+    private int x;
+    private int y;
+    private boolean baseIsObstacle;
+    private boolean overlayIsObstacle;
 
-    public Point(char marker, int x, int y, boolean isObstacle) {
-        this.marker = marker;
+    public Point(char baseMarker, int x, int y, boolean baseIsObstacle) {
+        this.baseMarker = baseMarker;
         this.x = x;
         this.y = y;
-        this.isObstacle = isObstacle;
+        this.baseIsObstacle = baseIsObstacle;
     }
 
-    public char getMarker() { return marker; }
+    public char getMarker() { return overlayMarker != null ? overlayMarker : baseMarker; }
+    public char getBaseMarker() { return baseMarker; }
+    public Character getOverlayMarker() { return overlayMarker; }
     public int getX() { return x; }
     public int getY() { return y; }
-    public boolean isObstacle() { return isObstacle; }
+    public boolean isObstacle() { return baseIsObstacle || overlayIsObstacle; }
+
+    public void setOverlay(char overlayMarker, boolean overlayIsObstacle) {
+        this.overlayMarker = overlayMarker;
+        this.overlayIsObstacle = overlayIsObstacle;
+    }
 }
