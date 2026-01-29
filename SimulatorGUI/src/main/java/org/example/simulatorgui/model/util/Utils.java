a23c2caa2201798a4ccdb01ab052ece1752cda45
mikolajdorosz <mikolajdorosz@gmail.com>
2026-01-30 00:53:48 +0100

    Removed some utis.

--- SimulatorGUI/src/main/java/org/example/simulatorgui/model/util/Utils.java
+++ SimulatorGUI/src/main/java/org/example/simulatorgui/model/util/Utils.java
@@ -8,12 +8,6 @@ import org.example.simulatorgui.model.car.Car;
 import java.util.Optional;
 
 public class Utils {
-    public static String formatTime(double seconds) {
-        int mins = (int) (seconds / 60);
-        int secs = (int) (seconds % 60);
-        int millis = (int) ((seconds - ((int) seconds)) * 1000); // milliseconds
-        return String.format("%02d:%02d:%03d", mins, secs, millis);
-    }
     public static TextFormatter<String> createDecimalTextFormatter() {
         return new TextFormatter<>(change -> {
             String newText = change.getControlNewText();
@@ -26,7 +20,6 @@ public class Utils {
             return newText.matches("\\d*") ? change : null;
         });
     }
-    public static double distance(Position a, Position b) { return Math.sqrt(Math.pow(b.getX() - a.getX(), 2) + Math.pow(b.getY() - a.getY(), 2)); }
     public static void showDuplicateAlert(String name, String item) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Duplicate " + name);
